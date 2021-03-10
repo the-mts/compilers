a81c898fc204b538b42737702aa6c2eef3edf435
Shashwat Gupta <shashujbp@gmail.com>
2021-03-10 13:42:36 +0530

    gitignore

--- tests/test5.c
+++ tests/test5.c
@@ -1,20 +1,5 @@
-int main(int argv[0])
-{
-	/*Checking 
-	Multiline after column*/int y=0;
-	int x = 0;
-	while(1)
-	{
-		if(x!=2)
-		{
-			continue;
-		}
-		else
-		{
-			break;
-		}
-	}
-	x = x+++y;
-	x = x+(++y);
-	x = x---y;
+/* #include<stdio.h> */
+int main(){
+	printf("%d\n", sizeof( int const ));
+	return 0;
 }
