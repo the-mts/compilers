e7202fe95144dcdbec395067777af7f0dd478fde
Akhilesh Sharma <dragnoid99@gmail.com>
2021-04-26 22:34:03 +0530

    minor changes

--- tests/test.c
+++ tests/test.c
@@ -1,16 +1,27 @@
 /*#include<stdio.h>*/
+void fun(int x[][10]){
+	x[1]++;
+}
+
 int main(){
-	unsigned int a = 8;
-	unsigned int b = 2;
+	int *x, y=2;
 	/*short int b = 5;*/
 	/*a &= b;*/
 	/*int c = a & b;*/ 
 	/*a+=10;*/
 	/*int e = a*++a;*/
 	/*int e = a ^ b;*/
-	int f = a >> b;
-	// printf("%d\n", f);	
-	return 0;
+	int xx[2][10];
+
+
+
+
+	x = &y;
+	fun(xx);/*
+	printf("%p %p", x, ++x);
+	printf("%p %p", x, ++x);*/
+	/*printf("%d\n", f);	
+	*/return 0;
 }
 /*[]
 CALL
