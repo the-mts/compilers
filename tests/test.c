022b21baafe75dd75dde3762b03fe716272fe57e
Shashwat Gupta <shashujbp@gmail.com>
2021-04-26 22:34:56 +0530

    codegen bugs

--- tests/test.c
+++ tests/test.c
@@ -1,60 +1,9 @@
 /*#include<stdio.h>*/
 int main(){
-	unsigned int a = 8;
-	unsigned int b = 2;
-	/*short int b = 5;*/
-	/*a &= b;*/
-	/*int c = a & b;*/ 
-	/*a+=10;*/
-	/*int e = a*++a;*/
-	/*int e = a ^ b;*/
-	int f = a >> b;
-	// printf("%d\n", f);	
-	return 0;
+	char c;
+	int x = 4;
+	short int b;
+	int y= x++;
+	// printf("%d\n", x);
+	// printf("%d\n", y);
 }
-/*[]
-CALL
-(-to-)
-PARAM
-++ (Pre/Post)
--- ''
-UNARY&
-UNARY*
-!
-UNARY+
-UNARY-
-~
-inttoreal
-*int
-*real
-/int
-/real
-%
-+int
-+real
--int
--real
-<<
->>
-<int
-<real
->int
->real
-<=int,real
->=int,real
-==int,real
-!=int,real
-&
-^
-|
-IF_TRUE_GOTO
-GOTO
-=
-+=int,real
--=...
-*=...
-/=...
-%,<<,>>,&,^,|=
-RETURN_VOID
-RETURN
-FUNC_START*/
